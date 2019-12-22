e5b1143d1e9c5141e1695dfb3faace18081b5795
confident-token-1019 <theoonetj@gmail.com>
2019-12-22 09:43:01 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,139 +1,158 @@
+# BUNDLE!!!!!
+
+# BIN/TICTACTOE
+# This is our main executable and will be how we run our game.
+
+# LIB/TIC_TAC_TOE.RB
+# Our main TicTacToe class will be defined here with all the data and logic required to play a game of tic tac toe via instances of TicTacToe.
+
+# SPEC
+# There are three test files that should be completed in order. 01_tic_tac_toe_spec.rb sets tests for our helper methods within TicTacToe. 02_play_spec.rb tests the main #play method. 03_cli_spec.rb tests the CLI.
+
+class TicTacToe
+  def initialize(board = nil)
+    @board = board || Array.new(9, " ")
+  end
+end 
+
+# The "pipes" || = or. This is saying that if the board returns nil, return an empty array.  So, either display the current board, or a new one.
+
+# WIN_COMBINATIONS within the body of TicTacToe
+
 WIN_COMBINATIONS = [
-[0,1,2],
-[3,4,5],
-[6,7,8],
-[0,4,8],
-[2,4,6],
-[0,3,6],
-[1,4,7],
-[2,5,8]
-]
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+  ]
+  
+  #display_board: not the instance variable @board
+  
+    def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+  
+  # input_index
+  
+  def input_to_index(user_input)
+    user_input.to_i - 1
+  end
+  
+  # move: Note that we deleted the boar arguement, and added @ to board.  For instance, #move was move(board, position, char), but now board is intialized, so it is a characteristic of TicTacToe, no need to have it as an argument.  So, #move became simply move(position, char).
+  
+  
+  def move(position, char)
+    @board[position] = char
+  end
+# For #move to work, we need to position_taken and valid_move
+  def position_taken?(index_i)
+    ((@board[index_i] == "X") || (@board[index_i] == "O"))
+  end
 
-def display_board
-  puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
-  puts "-----------"
-  puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
-  puts "-----------"
-  puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
-end
-
-def input_to_index(user_input)
-	user_input.to_i - 1
-end
-
-def move(index, value)
-  @board[index] = value
-end
-
-# Helper Method
-def position_taken?(index)
-   !(@board[index] == " ")
-end
-
-#valid_move?
-def valid_move?(index)
-	#if move is valid
-	if position_taken?(index) == false && index.between?(0,8) == true
-		return true
-	#if move is invalid
-	elsif position_taken?(index) == true
-		return false
-	elsif index.between?(0, 8) == false 
-		return false
-	end
-end
+  def valid_move?(index)
+    index.between?(0,8) && !position_taken?(index)
+  end
+  
 
 #turn_count
-def turn_count
-	turns = 0
-	@board.each do |turn|
-		if turn == "X"
-			turns += 1
-		elsif turn == "O"
-			turns += 1
-		end
-	end
-	return turns
-end
-
+  def turn_count
+    number_of_turns = 0
+    @board.each do |space|
+    if space == "X" || space == "O"
+        number_of_turns += 1
+    end
+  end
+  return number_of_turns
+  
 #current_player
-def current_player
-	return turn_count.even? == true ? "X" : "O"
-end
-
-#turn
+  def current_player
+    if turn_count % 2 == 0
+    "X"
+    else
+    "O"
+  end
+ 
+#turn 
 def turn
-	puts "Please enter 1-9:"
-	user_input = gets.chomp
-	index = input_to_index(user_input)
-	if valid_move?(index) == true && current_player == "X"
-			move(index, value = "X")
-			puts display_board
-	elsif valid_move?(index) == true && current_player == "O"
-			move(index, value = "O")
-			puts display_board
-	elsif valid_move?(index) == false
-		puts "Please enter 1-9:"
-		input = gets.chomp
-	else 
-		puts "Please enter 1-9:"
-		input = gets.chomp
-	end
+    puts "Please enter 1-9:"
+    input = gets.strip
+    index = input_to_index(input)
+    char = current_player
+    if valid_move?(index)
+      move(index, char)
+      display_board
+    else
+      turn
+    end
 end
 
 #won?
 def won?
-  WIN_COMBINATIONS.detect do |combo|
-    position_taken?(combo[0]) && @board[combo[0]] == @board[combo[1]] && @board[combo[1]] == @board[combo[2]]
+  WIN_COMBINATIONS.detect do |win_combo|
+    if (@board[win_combo[0]]) == "X" && (@board[win_combo[1]]) == "X" && (@board[win_combo[2]]) == "X"
+      return win_combo
+    elsif (@board[win_combo[0]]) == "O" && (@board[win_combo[1]]) == "O" && (@board[win_combo[2]]) == "O"
+      return win_combo
+    end
+      false
   end
 end
 
-#full
+#full?
 def full?
-	@board.all? do |index|
-		 index == "X" || index == "O"
-		end
+  @board.all?{|occupied| occupied != " "}
 end
 
 #draw
 def draw?
-		if won? 
-			return false
-		elsif full? == false 
-			return false 
-		else 
-			return true
-		end
+  !(won?) && (full?)
 end
 
-#over
+#over?
 def over?
-		if won? || draw? == true
-			return true
-		else
-			return false
-		end
+  won? || full? || draw?
 end
 
-#winner
+#winner?
 def winner
-	winning_combo = won?
-	 if winning_combo
-	    return @board[winning_combo[0]]
-	 end
+  WIN_COMBINATIONS.detect do |win_combo|
+    if (@board[win_combo[0]]) == "X" && (@board[win_combo[1]]) == "X" && (@board[win_combo[2]]) == "X"
+      return "X"
+    elsif (@board[win_combo[0]]) == "O" && (@board[win_combo[1]]) == "O" && (@board[win_combo[2]]) == "O"
+      return "O"
+    else
+      nil
+    end
+  end
+end
 end
 
 #play
 def play
-    while over? == false
-    	turn
-    end
-
-    if draw?
-    	puts "Cat's Game!"
-    elsif winner == "X" 
-    	puts "Congratulations X!"
-    elsif winner == "O"
-    	puts "Congratulations O!"
-    end
+  while over? == false
+    turn
+  end
+  if won?
+    puts "Congratulations #{winner}!"
+  elsif draw?
+    puts "Cat's Game!"
+  end
 end
+
+end
+
+
+#bin/tictactoe
+#require 'pry'
+#require_relative '../lib/tic_tac_toe.rb'
+
+#game = TicTacToe.new
+#game.play
