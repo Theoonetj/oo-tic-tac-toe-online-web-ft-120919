8e7bddf00a14d00427b26dd6080c8d29fd9edfa6
confident-token-1019 <theoonetj@gmail.com>
2019-12-22 09:40:46 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,19 +1,13 @@
-class TicTacToe
-
-  WIN_COMBINATIONS = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [6, 4, 2]
-  ]
-
-def initialize
-  @board = Array.new(9, " ")
-end
+WIN_COMBINATIONS = [
+[0,1,2],
+[3,4,5],
+[6,7,8],
+[0,4,8],
+[2,4,6],
+[0,3,6],
+[1,4,7],
+[2,5,8]
+]
 
 def display_board
   puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
@@ -24,118 +18,122 @@ def display_board
 end
 
 def input_to_index(user_input)
-  user_input.to_i - 1
+	user_input.to_i - 1
 end
 
-def move(index, current_player = "X")
-  @board[index] = current_player
+def move(index, value)
+  @board[index] = value
 end
 
+# Helper Method
 def position_taken?(index)
-  !(@board[index].nil? || @board[index] == " ")
+   !(@board[index] == " ")
 end
 
+#valid_move?
 def valid_move?(index)
-  index.between?(0,8) && !position_taken?(index)
+	#if move is valid
+	if position_taken?(index) == false && index.between?(0,8) == true
+		return true
+	#if move is invalid
+	elsif position_taken?(index) == true
+		return false
+	elsif index.between?(0, 8) == false 
+		return false
+	end
 end
 
+#turn_count
 def turn_count
-  turn = 0
-  @board.each do |index|
-    if index == "X" || index == "O"
-      turn += 1
-    end
-  end
-  return turn
+	turns = 0
+	@board.each do |turn|
+		if turn == "X"
+			turns += 1
+		elsif turn == "O"
+			turns += 1
+		end
+	end
+	return turns
 end
 
+#current_player
 def current_player
-  #if the turn count is an even number, that means O just went, so the next/current player is X
-  num_turns = turn_count
-  if num_turns % 2 == 0
-    player = "X"
-  else
-    player = "O"
-  end
-  return player
+	return turn_count.even? == true ? "X" : "O"
 end
 
+#turn
 def turn
-  puts "Please choose a number 1-9:"
-  user_input = gets.chomp
-  index = input_to_index(user_input)
-  if valid_move?(index)
-    player_token = current_player
-    move(index, player_token)
-    display_board
-  else
-    turn
-  end
+	puts "Please enter 1-9:"
+	user_input = gets.chomp
+	index = input_to_index(user_input)
+	if valid_move?(index) == true && current_player == "X"
+			move(index, value = "X")
+			puts display_board
+	elsif valid_move?(index) == true && current_player == "O"
+			move(index, value = "O")
+			puts display_board
+	elsif valid_move?(index) == false
+		puts "Please enter 1-9:"
+		input = gets.chomp
+	else 
+		puts "Please enter 1-9:"
+		input = gets.chomp
+	end
 end
 
+#won?
 def won?
-  WIN_COMBINATIONS.each {|win_combo|
-    index_0 = win_combo[0]
-    index_1 = win_combo[1]
-    index_2 = win_combo[2]
-
-    position_1 = @board[index_0]
-    position_2 = @board[index_1]
-    position_3 = @board[index_2]
-
-    if position_1 == "X" && position_2 == "X" && position_3 == "X"
-      return win_combo
-    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
-      return win_combo
-    end
-  }
-  return false
+  WIN_COMBINATIONS.detect do |combo|
+    position_taken?(combo[0]) && @board[combo[0]] == @board[combo[1]] && @board[combo[1]] == @board[combo[2]]
+  end
 end
 
+#full
 def full?
-  @board.all? {|index| index == "X" || index == "O"}
+	@board.all? do |index|
+		 index == "X" || index == "O"
+		end
 end
 
+#draw
 def draw?
-  if !won? && full?
-    return true
-  else
-    return false
-  end
+		if won? 
+			return false
+		elsif full? == false 
+			return false 
+		else 
+			return true
+		end
 end
 
+#over
 def over?
-  if won? || draw?
-    return true
-  else
-    return false
-  end
+		if won? || draw? == true
+			return true
+		else
+			return false
+		end
 end
 
+#winner
 def winner
-  index = []
-  index = won?
-  if index == false
-    return nil
-  else
-    if @board[index[0]] == "X"
-      return "X"
-    else
-      return "O"
-    end
-  end
+	winning_combo = won?
+	 if winning_combo
+	    return @board[winning_combo[0]]
+	 end
 end
 
+#play
 def play
-  until over? == true
-    turn
-  end
+    while over? == false
+    	turn
+    end
 
-  if won?
-    puts "Congratulations #{winner}!"
-  elsif draw?
-    puts "Cat's Game!"
-  end
-end
-
+    if draw?
+    	puts "Cat's Game!"
+    elsif winner == "X" 
+    	puts "Congratulations X!"
+    elsif winner == "O"
+    	puts "Congratulations O!"
+    end
 end
